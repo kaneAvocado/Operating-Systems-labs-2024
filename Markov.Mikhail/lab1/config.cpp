d78827266e0ab96bf77cb12786c72643d3e9b206
eagerbeaver04 <mike.markov04@gmail.com>
2024-10-02 19:51:48 +0300

    fix: config format

--- Markov.Mikhail/lab1/config.cpp
+++ Markov.Mikhail/lab1/config.cpp
@@ -1,6 +1,39 @@
 #include <cstring>
 #include "config.hpp"
 
+
+Data parse_line(const std::string& line)
+{
+    char separator = '"';
+    auto it1 = std::find(line.begin(), line.end(), separator);
+
+    if (it1 == line.end())
+        throw std::runtime_error("line format in config file is incorrect!");
+
+    auto it2 = std::find(it1+1, line.end(), separator);
+
+    if (it2 == line.end())
+        throw std::runtime_error("line format in config file is incorrect!");
+
+    std::string folder1 = std::string(it1+1, it2);
+
+    it1 = std::find(it2+1, line.end(), separator);
+
+    if (it1 == line.end())
+        throw std::runtime_error("line format in config file is incorrect!");
+
+    it2 = std::find(it1+1, line.end(), separator);
+
+    if (it2 == line.end())
+        throw std::runtime_error("line format in config file is incorrect!");
+
+    std::string folder2 = std::string(it1+1,it2);
+
+    int time = std::stoi(std::string(it2+1, line.end()));
+
+    return {folder1, folder2, time};
+}
+
 std::vector<Data> Config::read()
 {
     std::ifstream in_file(path);
@@ -14,10 +47,15 @@ std::vector<Data> Config::read()
 
     while (std::getline(in_file, line))
     {
-        std::istringstream ss(line);
-        Data entry;
-        ss >> entry.folder1 >> entry.folder2 >> entry.time;
-        data.push_back(entry);
+        try
+        {
+            data.emplace_back(parse_line(line));
+        }
+        catch (const std::exception &e)
+        {
+            syslog(LOG_ERR, "Error while parsing %s: %s", line.c_str(), e.what());
+            exit(EXIT_FAILURE);
+        }
     }
     in_file.close();
     syslog(LOG_INFO, "Config file %s was read successfully!", path.c_str());
